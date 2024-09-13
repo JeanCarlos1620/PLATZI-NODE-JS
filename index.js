5f6d6175424f12c8ef552832f9ad26235751c0f8
Jean Sanchez <SancheJC@modipsa.local>
2024-09-13 12:50:17 -0500

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const routerApi = require('./routes');
+const routerApi = require('./api/routes');
 const cors = require('cors');
-const { errorHandler, logErrors, boomErrorHandler } = require('./middlewares/errorHandler');
+const { errorHandler, logErrors, boomErrorHandler } = require('./api/middlewares/errorHandler');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -20,11 +20,11 @@ const options = {
 
 app.use(cors(options));
 
-app.get('/', (req, res) => {
+routerApi(app);
+
+app.get('/api', (req, res) => {
     res.send('Hola mi primer server');
 })
-
-// routerApi(app);
 
 app.use(logErrors);
 app.use(boomErrorHandler);
