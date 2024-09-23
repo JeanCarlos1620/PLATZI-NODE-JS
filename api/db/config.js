5a472e0567f5c3396dbe208eb81db9f5033345bc
Jean Sanchez <SancheJC@modipsa.local>
2024-09-23 12:05:13 -0500

    deploy

--- api/db/config.js
+++ api/db/config.js
@@ -1,16 +1,32 @@
 const { config } = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const USER = encodeURIComponent(config.dbUser);
+// const PASSWORD = encodeURIComponent(config.dbPassword);
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+// module.exports = {
+//     development: {
+//         url: URI,
+//         dialect: 'postgres',
+//     },
+//     production: {
+//         url: URI,
+//         dialect: 'postgres',
+//     }
+// };
 
 module.exports = {
     development: {
-        url: URI,
-        dialect: 'postgres',
+        url: config.dbUrl,
+        dialect: config.dbEngine
     },
     production: {
-        url: URI,
-        dialect: 'postgres',
+        url: config.dbUrl,
+        dialect: config.dbEngine, // postgres,
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false,
+            }
+        }
     }
-};
+}
--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -6,16 +6,28 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
-    logging: console.log,
+// const sequelize = new Sequelize(URI, {
+//     dialect: 'postgres',
+//     logging: console.log,
 
-});
+// });
 
-setupModels(sequelize);
+// setupModels(sequelize);
 
 // se empieza a leer los modelos y crear las tablas 
 // no se recomienda usarlo en produccion
 // sequelize.sync();
+
+const options = {
+    dialect: config.dbEngine,
+    logging: config.isProd ? false : console.log,
+};
+
+if (config.isProd) {
+    options.dialectModule = require('pg');
+}
+
+const sequelize = new Sequelize(config.dbUrl, options)
+
 
 module.exports = sequelize;
