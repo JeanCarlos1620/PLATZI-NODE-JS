84032743e6d14e832a2aa5b876c58288de704579
Jean Sanchez <SancheJC@modipsa.local>
2024-09-23 16:22:09 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const routerApi = require('./routes');
 const cors = require('cors');
 const { errorHandler, logErrors, boomErrorHandler, ormErrorHandler } = require('./middlewares/errorHandler');
 const app = express();
-const port = process.env.DB_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
