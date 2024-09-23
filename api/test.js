7850ad4fbcd6d08754f26dd370f2da3cb113b9f3
Jean Sanchez <SancheJC@modipsa.local>
2024-09-23 16:26:45 -0500

    deploy

---
+++ api/test.js
@@ -0,0 +1,9 @@
+const sequelize = require('./libs/sequelize'); // Ajusta la ruta según tu estructura de carpetas
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexión a la base de datos establecida con éxito.');
+    })
+    .catch(err => {
+        console.error('No se pudo conectar a la base de datos:', err);
+    });
